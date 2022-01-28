e87c3f248e9b075f857fd0a00eadf7c136c74f4d
himja <himja.jnext@gmail.com>
2022-01-28 18:23:02 +0530

    make in form dropdown menu, radio button, chekbox

--- my-app/src/MyComponents/FormComponent.js
+++ my-app/src/MyComponents/FormComponent.js
@@ -9,53 +9,82 @@ export default class Form extends React.Component {
     };
   }
   handleValidation() {
-    let fields = this.state.fields;
+    let { fields } = this.state;
     let error = {};
     let formIsvalid = true;
 
-    if (!fields["Name"]) {
-      formIsvalid = false;
-      error["Name"] = "*Name is requierd.";
+    // Name validation
+
+    // if (!fields["Name"]) {
+    //   formIsvalid = false;
+    //   error["Name"] = "*Name is required.";
+    // }
+
+    // if (typeof fields["Name"] !== "undefined") {
+    //   if (!fields["Name"].match(/^[a-zA-Z]+$/)) {
+    //     formIsvalid = false;
+    //     error["Name"] = "*Only letters allowed.";
+    //   }
+    // }
+
+    // Standard validation
+
+    // if (!fields["Std"]) {
+    //   formIsvalid = false;
+    //   error["Std"] = "*Standard is required.";
+    // }
+    // if (typeof fields["Std"] !== "undefined") {
+    //   if (!fields["Std"].match(/^[0-9\b]+$/)) {
+    //     formIsvalid = false;
+    //     error["Std"] = "*Only Numbers allowed.";
+    //   }
+    // }
+
+    // Age validation
+
+    // if (!fields["Age"]) {
+    //   formIsvalid = false;
+    //   error["Age"] = "*Age is required.";
+    // }
+    // if (typeof fields["Age"] !== "undefined") {
+    //   if (!fields["Age"].match(/^[0-9\b]+$/)) {
+    //     formIsvalid = false;
+    //     error["Age"] = "*Only Numbers allowed.";
+    //   }
+    // }
+
+    // AvgMarks validation
+
+    // if (!fields["AvgMarks"]) {
+    //   formIsvalid = false;
+    //   error["AvgMarks"] = "*AvgMarks is required.";
+    // }
+    // if (typeof fields["AvgMarks"] !== "undefined") {
+    //   if (!fields["AvgMarks"].match(/^[0-9\b]+$/)) {
+    //     formIsvalid = false;
+    //     error["AvgMarks"] = "*Only Numbers allowed.";
+    //   }
+    // }
+
+    // Dropdown validation
+
+    // if (fields["course"] !== "undefined") {
+    //   formIsvalid = false;
+    //   error["dropdown"] = "*tHIS is required.";
+    // }
+
+    // Radiobutton validation
+
+    if (fields["radiobutton"] == "defined") {
+      formIsvalid = true;
     }
 
-    if (typeof fields["Name"] !== "undefined") {
-      if (!fields["Name"].match(/^[a-zA-Z]+$/)) {
-        formIsvalid = false;
-        error["Name"] = "*Only letters allowed.";
-      }
-    }
+    // Checkbox validation
 
-    if (!fields["Std"]) {
-      formIsvalid = false;
-      error["Std"] = "*Standard is requierd.";
-    }
-    if (typeof fields["Std"] !== "undefined") {
-      if (!fields["Std"].match(/^[0-9\b]+$/)) {
-        formIsvalid = false;
-        error["Std"] = "*Only Numbers allowed.";
-      }
-    }
-
-    if (!fields["Age"]) {
-      formIsvalid = false;
-      error["Age"] = "*Age is requierd.";
-    }
-    if (typeof fields["Age"] !== "undefined") {
-      if (!fields["Age"].match(/^[0-9\b]+$/)) {
-        formIsvalid = false;
-        error["Age"] = "*Only Numbers allowed.";
-      }
-    }
-    if (!fields["AvgMarks"]) {
-      formIsvalid = false;
-      error["AvgMarks"] = "*AvgMarks is requierd.";
-    }
-    if (typeof fields["AvgMarks"] !== "undefined") {
-      if (!fields["AvgMarks"].match(/^[0-9\b]+$/)) {
-        formIsvalid = false;
-        error["AvgMarks"] = "*Only Numbers allowed.";
-      }
-    }
+    // if (!fields["course"]) {
+    //   formIsvalid = false;
+    //   error["course"] = "*This is required.";
+    // }
 
     this.setState({ error: error });
     return formIsvalid;
@@ -75,11 +104,12 @@ export default class Form extends React.Component {
     let fields = this.state.fields;
     fields[field] = e.target.value;
     this.setState({ fields });
+    console.log("hello", fields);
   }
 
   render() {
     const { singleUserinfo } = this.props;
-    // console.log("singleUserinfo", singleUserinfo);
+    console.log("singleUserinfo", singleUserinfo);
     return (
       <div>
         <form
@@ -162,6 +192,74 @@ export default class Form extends React.Component {
             />
             <p style={{ color: "rgb(231, 111, 5)" }}>
               {this.state.error["AvgMarks"]}
+            </p>
+          </div>
+          <div className="datablock">
+            <select
+              id="course"
+              name="course"
+              onChange={
+                ((e) => this.props.handleInputChange(e),
+                this.handleChange.bind(this, "course"))
+              }
+            >
+              <option value>Please Select Your Course--</option>
+              <option value={singleUserinfo.course}>HTML</option>
+              <option value={singleUserinfo.course}>CSS</option>
+              <option value={singleUserinfo.course}>JavaScript</option>
+              <option value={singleUserinfo.course}>Reactjs</option>
+              <option value={singleUserinfo.course}>Angular</option>
+            </select>
+            <p style={{ color: "rgb(231, 111, 5)" }}>
+              {this.state.error["dropdown"]}
+            </p>
+          </div>
+
+          <div className="radioblock">
+            <p>Choose a favorite Web language: </p>
+            <input
+              type="radio"
+              Name="lang"
+              id="html"
+              value="html"
+              onChange={
+                ((e) => this.props.handleInputChange(e),
+                this.handleChange.bind(this, "lang"))
+              }
+            />
+            <lable htmlFor="html">HTML</lable>
+            <input
+              type="radio"
+              Name="lang"
+              id="css"
+              value="css"
+              onChange={this.handleChange.bind(this, "lang")}
+            />
+            <lable htmlFor="css">CSS</lable>
+            <input
+              type="radio"
+              Name="lang"
+              id="java"
+              value="JavaScript"
+              onChange={this.handleChange.bind(this, "lang")}
+            />
+            <lable htmlFor="java">JavaScript</lable>
+            <p style={{ color: "rgb(231, 111, 5)" }}>
+              {this.state.error["radiobutton"]}
+            </p>
+          </div>
+          <div className="datablock">
+            <p name="hobby">Select your Hobbies: </p>
+            <input type="checkbox" Name="Sport" id="Sport" />
+            <lable htmlFor="Sport">Sports</lable>
+            <input type="checkbox" Name="Cooking" id="Cooking" />
+            <lable htmlFor="Cooking">Cooking</lable>
+            <input type="checkbox" Name="Reading" id="Reading" />
+            <lable htmlFor="Reading">Reading</lable>
+            <input type="checkbox" Name="Singing" id="Singing" />
+            <lable htmlFor="Singing">Singing</lable>
+            <p style={{ color: "rgb(231, 111, 5)" }}>
+              {this.state.error["hobby"]}
             </p>
           </div>
           <button type="submit" className="sub-btn">
--- my-app/src/MyComponents/Table.js
+++ my-app/src/MyComponents/Table.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Table extends React.Component {
   render() {
     const { usertabVal } = this.props;
-    console.log("usertabVal", usertabVal);
+    // console.log("usertabVal", usertabVal);
     return (
       <div>
         <table className="table">
@@ -13,17 +13,23 @@ export default class Table extends React.Component {
               <th>Std</th>
               <th>Age</th>
               <th>AvgMarks</th>
+              <th>Course</th>
+              <th>Language</th>
+              <th>Hobby</th>
             </tr>
           </thead>
           <tbody>
             {usertabVal?.map((item, index) => {
-              // console.log("item", item);
+              console.log("item", item);
               return (
                 <tr onClick={() => this.props.handleClick(item)}>
                   <td>{item?.Name}</td>
                   <td>{item?.Std}</td>
                   <td>{item?.Age}</td>
                   <td>{item?.AvgMarks}</td>
+                  <td>{item?.course}</td>
+                  <td>{item?.lang}</td>
+                  <td>{item?.hobby}</td>
                 </tr>
               );
             })}
